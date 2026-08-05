8805ef082ad90323ce4732a43451cac59cff7af8
jack <czyy2025@gmail.com>
2026-08-05 19:36:15 +0800

    release: prepare version 1.0.2

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QApplication::setApplicationName(QStringLiteral("Modbus 通讯助手"));
-    QApplication::setApplicationVersion(QStringLiteral("1.0.1"));
+    QApplication::setApplicationVersion(QStringLiteral("1.0.2"));
     QApplication::setOrganizationName(QStringLiteral("LocalTools"));
     QLocale::setDefault(QLocale::Chinese);
 
